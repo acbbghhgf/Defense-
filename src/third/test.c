9387caee6032ab48effdf87f85081f7b9f444869
ww <490165508@qq.com>
2019-11-15 01:01:38 -0800

    fix Makefile and add test demo

--- src/third/test.c
+++ src/third/test.c
@@ -6,9 +6,54 @@
  ************************************************************************/
 
 #include "core/wwk_type.h"
-
-int main()
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+ 
+#define MAXLINE 80
+#define SERV_PORT 8000
+ 
+int main(int argc, char *argv[])
 {
-    wwk_debug("test success.\n");
+    struct sockaddr_in servaddr;
+    char buf[MAXLINE];
+ 
+    int sockfd, n;
+    char *str;
+    int ret = 0;
+ 
+    if(argc != 2) {
+        fputs("usage: ./client message\n", stderr);
+        exit(1);
+    }
+    
+    str = argv[1];
+ 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr);
+    servaddr.sin_port = htons(SERV_PORT);
+    
+    connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+   
+    ret = write(sockfd, str, strlen(str));
+    if(ret != strlen(str))
+    {
+        wwk_debug("write faile , send data len : %d \n", ret);
+    }
+ 
+    n = read(sockfd, buf, MAXLINE);
+    printf("Response from server:\n");
+    ret = write(STDOUT_FILENO, buf, n);
+    if(ret != n)
+    {
+        wwk_debug("write faile , send data len : %d \n", ret);
+    }
+ 
+    close(sockfd);
     return 0;
 }
