0b08cc0d9004206571b42a7ba3b109654ea4688a
ww <490165508@qq.com>
2019-11-15 00:49:26 -0800

    fix makefile and add test demo in third

---
+++ src/third/test.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: test.c
+	> Author: 
+	> Mail: 
+	> Created Time: Fri 15 Nov 2019 12:38:23 AM PST
+ ************************************************************************/
+
+#include "core/wwk_type.h"
+
+int main()
+{
+    wwk_debug("test success.\n");
+    return 0;
+}
